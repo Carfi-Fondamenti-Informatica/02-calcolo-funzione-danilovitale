e95dd9e69c2dda0ec0ce44a15b397756270cd37b
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-11-14 10:08:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+  float a = 0, b = 0, x = 0, y = 0, z = 0; //inizializzo le variabili
+  cin >> a >> b >> x >> y;
+  if (x < 0 && y > 0) //controllo i casi come da consegna
+    {
+      z = a * x - b * y;
+      cout << "\n" << z << endl; //stampo in caso ci siano le condizioni
+    }
+  else if (x >= 0 && y <= 0)
+    {
+      z = a * (x * x) - b * y; 
+      cout << "\n" << z << endl;
+    }
+  else
+    {
+      z = a * x + b * (y * y);
+      cout << "\n" << z << endl;
+    }
    return 0;
 }
